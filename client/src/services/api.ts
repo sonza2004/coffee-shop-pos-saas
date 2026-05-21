1562a8a90f89c75365efcd9298ec3a19ff902828
sonza2004 <81893610+sonza2004@users.noreply.github.com>
2026-05-22 00:01:07 +0700

    frontend: add axios API client with JWT interceptor

---
+++ client/src/services/api.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+
+// Axios instance
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Attach JWT token automatically
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Global response handler
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+// Typed API helpers (basic layer)
+export const get = <T>(url: string) => api.get<T>(url).then(res => res.data);
+export const post = <T>(url: string, data?: any) => api.post<T>(url, data).then(res => res.data);
+export const patch = <T>(url: string, data?: any) => api.patch<T>(url, data).then(res => res.data);
+export const del = <T>(url: string) => api.delete<T>(url).then(res => res.data);
